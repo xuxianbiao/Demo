911c191e43725617b2fe9c116cd6f5e42c526e7e
xuxb <497238688@qq.com>
2018-10-25 13:51:06 +0800

    啦啦啦

--- app/src/main/java/com/example/dell/demo/MainActivity.java
+++ app/src/main/java/com/example/dell/demo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("lalal","hhh3");
+        Log.i("lalal","hhh4");
     }
 }
